d5d563d3640ad44b897f27bc303002c6d3a0c165
coding-jana <jana63sc@gmail.com>
2025-12-05 16:31:58 +0100

    read account

--- service/routes.py
+++ service/routes.py
@@ -81,7 +81,15 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id)
+    app.logger.info("Request to update account: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account [{account_id}] couldn't be found")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
